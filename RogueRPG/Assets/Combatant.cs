5075be56accbeb09fecce3c58df00f0268ecfd8f
Elton Mesquita <callmeelton@gmail.com>
2018-03-22 17:23:02 -0300

    First Blood

---
+++ RogueRPG/Assets/Combatant.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combatant : MonoBehaviour {
+
+	public float hp = 10;
+	public bool playable = false;
+	
+
+	public void Attack (Combatant c){
+		c.hp -= 1;
+		print (c.hp);
+	}
+
+	public void SuperAttack (Combatant c){
+		c.hp -= 2;
+		print (c.hp);
+	}
+}
