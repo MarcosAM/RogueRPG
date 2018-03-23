235870d369e2ad432f5358caa543e2c118e3c885
Elton Mesquita <callmeelton@gmail.com>
2018-03-23 17:11:40 -0300

    White Background

---
+++ RogueRPG/Assets/Skill.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill : MonoBehaviour {
+
+	string name;
+	enum SkillTypes {PhysicalAtk};
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
