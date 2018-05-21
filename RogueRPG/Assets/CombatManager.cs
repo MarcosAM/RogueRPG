764d0040e831c2bb817e8c0d79a67dc19c9a05bc
DESKTOP-K49PGQT\MarcosMourão <marcosarruda.m@gmail.com>
2018-05-20 21:27:19 -0300

    Initiative by List Instead of Array

--- RogueRPG/Assets/CombatManager.cs
+++ RogueRPG/Assets/CombatManager.cs
@@ -5,10 +5,14 @@ using System;
 
 public class CombatManager : MonoBehaviour {
 
-	[SerializeField]private Combatant[] initiativeList;
+//	[SerializeField]private Combatant[] initiativeList;
+	[SerializeField]private List<Combatant> initiativeOrder;
 	private int round;
 	private int activeCombatant;
-	private bool isReachargingEnergy=false;
+	private bool WaitingForCombatantsToRechargeEnergy = false;
+
+	private Combatant[] heroeParty;
+	private Combatant[] enemieParty;
 
 	[SerializeField]private Party heroesParty;
 	[SerializeField]private Party enemiesParty;
@@ -16,65 +20,54 @@ public class CombatManager : MonoBehaviour {
 	private CombHUDManager combatantHUDManager;
 
 	void Start () {
-		//TODO Ajustar initiativeList e Party para caso os inimigos comecem a dar spawn em outros inimigos.
-		initiativeList = FindObjectsOfType<Combatant> ();
+		FillInitiativeOrderWithAllCombatants();
+//		initiativeList = FindObjectsOfType<Combatant> ();
 		round = 0;
 		activeCombatant = 0;
+
+		FillPartiesWithCombatants();
+//		TODO Será que e deveria substituir objetos da classe Party por apenas arrays de combatants
 		heroesParty.Initialize(false);
 		enemiesParty.Initialize(true);
+
 		combatantHUDManager = FindObjectOfType<CombHUDManager>();
 		combatantHUDManager.InitializeCombatantHUDs(heroesParty.getCombatants(),enemiesParty.getCombatants());
 		StartTurn ();
 	}
 
 	void NextTurn(){
-		initiativeList[activeCombatant] = null;
-		round++;
-		activeCombatant = round % initiativeList.Length;
-		StartTurn ();
+		if(initiativeOrder.Count>0){
+			initiativeOrder.RemoveAt(0);
+			round++;
+			StartTurn ();
+		}
 	}
 
 	IEnumerator RechargeEnergy (){
-		while (isReachargingEnergy){
+		while (WaitingForCombatantsToRechargeEnergy){
 			EventManager.RechargeEnergy(0.1f);
 			yield return new WaitForSeconds(0.1f);
 		}
 	}
 
-	void AddToInitiative (Combatant c){
-		int i = round;
-		while (initiativeList[i%initiativeList.Length]!=null){
-			i++;
+	void AddToInitiative (Combatant combatant){
+		initiativeOrder.Add(combatant);
+		if(WaitingForCombatantsToRechargeEnergy = true){
+			WaitingForCombatantsToRechargeEnergy = false;
+			StartTurn();
 		}
-		initiativeList[i%initiativeList.Length] = c;
-		isReachargingEnergy = false;
-		StartTurn();
 	}
 
-	void DeleteFromInitiative (Combatant c){
-		bool found = false;
-		for(int i = 1; i<initiativeList.Length;i++){
-			if(initiativeList[(round+i)%initiativeList.Length] == null){
-				break;
-			}
-			if(initiativeList[(round+i)%initiativeList.Length] != null && found){
-				initiativeList [((round + i) % initiativeList.Length) - 1] = initiativeList [(round + i) % initiativeList.Length];
-				initiativeList [(round + i) % initiativeList.Length] = null;
-			}
-			if(initiativeList[(round+i)%initiativeList.Length] == c && !found){
-				initiativeList [(round + i) % initiativeList.Length] = null;
-				found = true;
-			}
-		}
+	void DeleteFromInitiative (Combatant combatant){
+		initiativeOrder.Remove(combatant);
 	}
 
 	void StartTurn ()
 	{
-		if (initiativeList [activeCombatant] != null) {
-			initiativeList [activeCombatant].StartTurn ();
-		}
-		else {
-			isReachargingEnergy = true;
+		if (initiativeOrder.Count>0) {
+			initiativeOrder [0].StartTurn ();
+		} else {
+			WaitingForCombatantsToRechargeEnergy = true;
 			StartCoroutine(RechargeEnergy());
 		}
 	}
@@ -85,6 +78,18 @@ public class CombatManager : MonoBehaviour {
 			print ("Heróis ganharam!");
 		} else {
 			print ("Vilões ganharam!");
+		}
+	}
+
+	void FillPartiesWithCombatants(){
+		heroeParty = FindObjectsOfType<Hero>();
+		enemieParty = FindObjectsOfType<Enemy>();
+	}
+
+	void FillInitiativeOrderWithAllCombatants (){
+		Combatant[] temporaryList = FindObjectsOfType<Combatant>();
+		for(int i = 0; i<temporaryList.Length; i++){
+			initiativeOrder.Add(temporaryList[i]);
 		}
 	}
 
@@ -101,5 +106,4 @@ public class CombatManager : MonoBehaviour {
 		EventManager.OnDeathOf -= DeleteFromInitiative;
 		EventManager.OnPartyLost -= EndCombat;
 	}
-
 }
--- RogueRPG/Assets/Combatant.cs
+++ RogueRPG/Assets/Combatant.cs
@@ -9,7 +9,7 @@ public abstract class Combatant : MonoBehaviour {
 	[SerializeField]protected string myName;
 	protected int hp = 100;
 	protected int maxHp = 100;
-	protected float energy = 0;
+	[SerializeField]protected float energy = 0;
 	[SerializeField]protected bool alive = true;
 
 	[SerializeField]protected int atk, atkm, def, defm;
@@ -125,6 +125,7 @@ public abstract class Combatant : MonoBehaviour {
 	public void RecoverEnergy (float amount){
 		if(alive){
 			energy += amount;
+			print(myName + " está recarregnado energia");
 			RefreshHUD();
 			if(energy>=0){
 				EventManager.RechargedEnergy(this);
