8f3e4be2383a9e1ad43e28b3079a272d7e070bb1
Elton Mesquita <callmeelton@gmail.com>
2018-07-06 07:59:37 -0300

    Start Implementing Zombie Behavior

---
+++ RogueRPG/Assets/Characters/Behaviors/ZombieBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieBehavior : CombatBehavior {
+
+	public override void StartTurn ()
+	{
+		base.StartTurn ();
+		character.StartTurn();
+		ChooseSkill ();
+		ChooseTarget ();
+	}
+
+	public void ChooseSkill ()
+	{
+		choosedSkill = character.skills[Random.Range(0,character.skills.Length)];
+	}
+
+	public void ChooseTarget ()
+	{
+
+	}
+
+	public override void UseSkill ()
+	{
+		base.UseSkill ();
+		EventManager.OnSkillUsed += EndTurn;
+		choosedSkill.Effect (character,targetTile);
+	}
+
+	void EndTurn(){
+		EventManager.OnSkillUsed -= EndTurn;
+		character.EndTurn ();
+	}
+}
