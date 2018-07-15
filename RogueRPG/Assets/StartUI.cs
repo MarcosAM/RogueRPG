a6cfc6b5edba711a26d703fdb143da92dc30d6bc
DESKTOP-K49PGQT\MarcosMourão <marcosarruda.m@gmail.com>
2018-07-15 10:47:19 -0300

    Restart Game but Really Buggy

---
+++ RogueRPG/Assets/StartUI.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartUI : MonoBehaviour {
+
+	public void startTutorial(){
+		FindObjectOfType<GameManager>().LoadDungeonScene();
+	}
+}
