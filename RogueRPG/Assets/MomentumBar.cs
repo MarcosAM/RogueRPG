9637735ea41ea5b8ea6533dec309c90f33807fdc
Elton Mesquita <callmeelton@gmail.com>
2018-07-17 08:48:17 -0300

    Momentum Bar Prototype

---
+++ RogueRPG/Assets/MomentumBar.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MomentumBar : MonoBehaviour {
+
+	float targetAmount = 0;
+	float currentDisplayedAmount = 0;
+	float maxAmount = 100;
+	[SerializeField]float fillSpeed;
+	bool filling = false;
+	[SerializeField]Slider momentumBar;
+
+	public void addMomentum (float value){
+		targetAmount += value;
+		if (targetAmount > maxAmount)
+			targetAmount = maxAmount;
+		if (targetAmount < 0)
+			targetAmount = 0;
+		if(!filling){
+			StartCoroutine (fillBar());
+		}
+	}
+
+	void Update(){
+		if(Input.GetMouseButtonUp(0)){
+			print ("Aumenta!");
+			addMomentum (10);
+		}
+		if (Input.GetMouseButtonUp (1)) {
+			print ("Diminui!");
+			addMomentum (-10);
+		}
+	}
+
+	IEnumerator fillBar(){
+		filling = true;
+		while (currentDisplayedAmount != targetAmount) {
+			currentDisplayedAmount = Mathf.MoveTowards (currentDisplayedAmount,targetAmount,Time.deltaTime * fillSpeed);
+			momentumBar.value = currentDisplayedAmount / maxAmount;
+			yield return new WaitForSeconds (0);
+		}
+		filling = false;
+	}
+
+	public void Initialize(){
+		momentumBar = GetComponent<Slider> ();
+	}
+
+	void Awake(){
+		print ("Get Component");
+		Initialize ();
+	}
+}
