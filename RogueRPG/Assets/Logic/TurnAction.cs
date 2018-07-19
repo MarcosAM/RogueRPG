3462ff2d9771732c9d8814a5b661bb7049234d0b
Elton Mesquita <callmeelton@gmail.com>
2018-07-19 13:51:53 -0300

    TurnAction Started

---
+++ RogueRPG/Assets/Logic/TurnAction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TurnAction {
+
+	int priority;
+	float probability;
+
+	public virtual void defineEquipSkillTargetFor (CombatBehavior combatBehavior){}
+}
