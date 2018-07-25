15cc11304d3c339c5885aeb8a81fbb942826459a
Elton Mesquita <callmeelton@gmail.com>
2018-07-25 18:32:50 -0300

    Archer Behavior

--- RogueRPG/Assets/Characters/Behaviors/ArcherBehavior.cs
+++ RogueRPG/Assets/Characters/Behaviors/ArcherBehavior.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class ArcherBehavior : CombatBehavior {
 
+	void Start (){
+		TurnAction atkHealthier = new TAAtkHealthier (this);
+		TurnAction Retreat = new TARetreat (this);
+		possibleActions.Add (atkHealthier);
+		possibleActions.Add (Retreat);
+	}
+
 	public override void StartTurn ()
 	{
 		base.StartTurn ();
@@ -19,57 +26,67 @@ public class ArcherBehavior : CombatBehavior {
 
 	public void ChooseTarget ()
 	{
-		Battleground.Tile[] tempHeroesTiles = DungeonManager.getInstance ().getBattleground ().getHeroesTiles ();
-		Battleground.Tile[] tempEnemiesTiles = DungeonManager.getInstance ().getBattleground ().getEnemiesTiles ();
-
-		targetTile = tempEnemiesTiles[character.getPosition()];
-		if (tempHeroesTiles [character.getPosition ()].getOccupant () == null) {
-			for (int i = 0; i < tempHeroesTiles.Length; i++) {
-				if (targetTile != null) {
-					if (tempHeroesTiles [i].getOccupant () != null) {
-						if (tempHeroesTiles [i].getOccupant ().getHp () > targetTile.getOccupant ().getHp ()) {
-							targetTile = tempHeroesTiles [i];
-						}
-					}
-				} else {
-					if (tempHeroesTiles [i].getOccupant () != null) {
-						targetTile = tempHeroesTiles [i];
-					}
-				}
-			}
-		} else {
-			int mostDistant = 0;
-			int currentDistance = 0;
-			int leftIndex = 0;
-			int rightIndex = 0;
-			int minIndex = character.getPosition () - choosedSkill.getAlliesEffect ().getRange ();
-			if(minIndex < 0)
-				minIndex = 0;
-			int maxIndex = character.getPosition () + choosedSkill.getAlliesEffect ().getRange ();
-			if (maxIndex >= tempEnemiesTiles.Length)
-				maxIndex = tempEnemiesTiles.Length - 1;
-			for (int i = 0; i < tempEnemiesTiles.Length; i++) {
-				currentDistance = 0;
-				for (int j = 0; j < tempHeroesTiles.Length; j++) {
-					leftIndex = i - j;
-					if (leftIndex < minIndex)
-						leftIndex = 0;
-					rightIndex = i + j;
-					if (rightIndex > maxIndex)
-						rightIndex = maxIndex;
-					if (tempHeroesTiles [leftIndex].getOccupant () != null || tempHeroesTiles [rightIndex].getOccupant () != null) {
-						break;
-					} else {
-						currentDistance++;
-					}
-				}
-				if(currentDistance > mostDistant || (currentDistance == mostDistant && (targetTile.getOccupant() != null && tempEnemiesTiles[i].getOccupant() == null))){
-					targetTile = tempEnemiesTiles[i];
-					mostDistant = currentDistance;
-				}
+//		Battleground.Tile[] tempHeroesTiles = DungeonManager.getInstance ().getBattleground ().getHeroesTiles ();
+//		Battleground.Tile[] tempEnemiesTiles = DungeonManager.getInstance ().getBattleground ().getEnemiesTiles ();
+//
+//		targetTile = tempEnemiesTiles[character.getPosition()];
+//		if (tempHeroesTiles [character.getPosition ()].getOccupant () == null) {
+//			for (int i = 0; i < tempHeroesTiles.Length; i++) {
+//				if (targetTile != null) {
+//					if (tempHeroesTiles [i].getOccupant () != null) {
+//						if (tempHeroesTiles [i].getOccupant ().getHp () > targetTile.getOccupant ().getHp ()) {
+//							targetTile = tempHeroesTiles [i];
+//						}
+//					}
+//				} else {
+//					if (tempHeroesTiles [i].getOccupant () != null) {
+//						targetTile = tempHeroesTiles [i];
+//					}
+//				}
+//			}
+//		} else {
+//			int mostDistant = 0;
+//			int currentDistance = 0;
+//			int leftIndex = 0;
+//			int rightIndex = 0;
+//			int minIndex = character.getPosition () - choosedSkill.getAlliesEffect ().getRange ();
+//			if(minIndex < 0)
+//				minIndex = 0;
+//			int maxIndex = character.getPosition () + choosedSkill.getAlliesEffect ().getRange ();
+//			if (maxIndex >= tempEnemiesTiles.Length)
+//				maxIndex = tempEnemiesTiles.Length - 1;
+//			for (int i = 0; i < tempEnemiesTiles.Length; i++) {
+//				currentDistance = 0;
+//				for (int j = 0; j < tempHeroesTiles.Length; j++) {
+//					leftIndex = i - j;
+//					if (leftIndex < minIndex)
+//						leftIndex = 0;
+//					rightIndex = i + j;
+//					if (rightIndex > maxIndex)
+//						rightIndex = maxIndex;
+//					if (tempHeroesTiles [leftIndex].getOccupant () != null || tempHeroesTiles [rightIndex].getOccupant () != null) {
+//						break;
+//					} else {
+//						currentDistance++;
+//					}
+//				}
+//				if(currentDistance > mostDistant || (currentDistance == mostDistant && (targetTile.getOccupant() != null && tempEnemiesTiles[i].getOccupant() == null))){
+//					targetTile = tempEnemiesTiles[i];
+//					mostDistant = currentDistance;
+//				}
+//			}
+//		}
+		for(int i = 0; i < possibleActions.Count;i++){
+			possibleActions [i].tryToDefineEquipSkillTargetFor();
+			if(choosedSkill != null && targetTile != null){
+				break;
 			}
 		}
-		character.getHUD ().UseSkillAnimation ();
+		if (targetTile == null) {
+			print ("Fuck!");
+		} else {
+			character.getHUD ().UseSkillAnimation ();
+		}
 	}
 
 	public override void UseSkill ()
