e31aa7c808355f7968ac9460318016b09ac47d30
Elton Mesquita <callmeelton@gmail.com>
2018-07-26 16:43:56 -0300

    Effect Prefab Begins

---
+++ RogueRPG/Assets/Skills/Scripts/Effect.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Effect : ScriptableObject {
+	public virtual void effect (Character target){}
+}
