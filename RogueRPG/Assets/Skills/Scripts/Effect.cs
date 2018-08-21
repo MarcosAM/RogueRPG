30e22350eae16280bef8ac7a0b8f0bd14b71aef8
DESKTOP-K49PGQT\MarcosMourão <marcosarruda.m@gmail.com>
2018-08-21 06:44:30 -0300

    Effect

--- RogueRPG/Assets/Skills/Scripts/Effect.cs
+++ RogueRPG/Assets/Skills/Scripts/Effect.cs
@@ -3,5 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class Effect : ScriptableObject {
+	protected int value;
+	protected SkillAnimation animation;
 	public virtual void effect (Character target){}
+
+	public int getValue () {return value;}
+	public SkillAnimation getSkillAnimation () {return animation;}
 }
