5b59cf932febb773782334b8349d832a85046614
Elton Mesquita <callmeelton@gmail.com>
2018-09-11 17:11:11 -0300

    Little Shit

--- RogueRPG/Assets/TargetBtn.cs
+++ RogueRPG/Assets/TargetBtn.cs
@@ -24,53 +24,7 @@ public class TargetBtn : CombatBtn, IPointerEnterHandler, IPointerExitHandler {
 		CombHUDManager.getInstance().onTargetBtnPressed(tile);
 	}
 
-	public void Appear (Character user, Skill skill)
-	{
-		//TODO Só aparecer quando tiver alvo para poder aparecer
-//		if(tile.getOccupant() != null){
-//			if (tile.getOccupant().isAlive ()) {
-//				switch(skill.getTargets()){
-//				case Skill.Targets.Allies:
-//					if (tile.getOccupant ().isPlayable () && Mathf.Abs (tile.getOccupant ().getPosition () - user.getPosition()) <= skill.getRange ()) {
-//						button.interactable = true;
-//						text.color = new Color (text.color.r, text.color.g, text.color.b, 1f);
-//					}
-//					break;
-//				case Skill.Targets.Enemies:
-//					if (!tile.getOccupant ().isPlayable () && Mathf.Abs (tile.getOccupant ().getPosition () - user.getPosition ()) <= skill.getRange ()) {
-//						button.interactable = true;
-//						text.color = new Color (text.color.r, text.color.g, text.color.b, 1f);
-//					}
-//					if (tile.getOccupant().isPlayable()) {
-//						if (tile.getOccupant() == user) {
-//							button.interactable = true;
-//							text.text = "Defender";
-//						} else {
-//							button.interactable = true;
-//							text.text = "Mover-se";
-//						}
-//					}
-//					break;
-//				case Skill.Targets.Self:
-//					if (tile.getOccupant () == user) {
-//						button.interactable = true;
-//						text.color = new Color (text.color.r, text.color.g, text.color.b, 1f);
-//					}
-//					break;
-//				default:
-//					button.interactable = true;
-//					text.color = new Color (text.color.r, text.color.g, text.color.b, 1f);
-//					break;
-//				}
-//			}
-//		}
-//		else {
-//			button.interactable = true;
-//			text.text = "Habilidade Secundária";
-//		}
-//		text.color = new Color (0.2f,0.2f,0.2f,1f);
-//		button.interactable = true;
-
+	public void Appear (Character user, Skill skill){
 		image.gameObject.SetActive (true);
 		if (tile.getOccupant () != null) {
 			if (user.isPlayable () == tile.isFromHero ()) {
