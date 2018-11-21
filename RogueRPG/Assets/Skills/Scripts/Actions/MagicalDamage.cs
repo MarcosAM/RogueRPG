7b08842b078607092c471421c5b9c2bc669174c4
Elton Mesquita <callmeelton@gmail.com>
2018-11-21 18:03:10 -0300

    Magical Damage

---
+++ RogueRPG/Assets/Skills/Scripts/Actions/MagicalDamage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicalDamage : Damage
+{
+    public override void TryToDamage(Character user, Character target, float attack)
+    {
+        if (attack > target.GetStatValue(Stat.Stats.Dodge))
+        {
+            target.LoseHpBy((int)(user.GetStatValue(Stat.Stats.Atkm) * Random.Range(1f, 1.2f) * dmgIntensifier - target.GetStatValue(Stat.Stats.Defm)), false);
+        }
+        else
+        {
+            Debug.Log("Missed!");
+        }
+    }
+}
--- RogueRPG/Assets/Skills/Scripts/Actions/MeleeAttack.cs
+++ RogueRPG/Assets/Skills/Scripts/Actions/MeleeAttack.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Actions/Attacks/Melee")]
 public class MeleeAttack : Attack
 {
-    public override bool IsTargetable(Character user, Tile tile) { return Mathf.Abs(user.GetPosition() - tile.GetRow()) <= range && tile.CharacterIsAlive(); }
+    public override bool IsTargetable(Character user, Tile tile) { return Mathf.Abs(user.GetPosition() - tile.GetRow()) <= range && tile.CharacterIsAlive() && user.IsPlayable() != tile.GetSide(); }
     public override bool WillBeAffected(Character user, Tile target, Tile tile) { return target = tile; }
 
     public override void Act(Character user, Tile target)
