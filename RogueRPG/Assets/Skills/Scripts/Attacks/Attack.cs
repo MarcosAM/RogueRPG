27c06457da8c4ce7e3a08be0c071dea672d0d3c2
DESKTOP-K49PGQT\MarcosMourão <marcosarruda.m@gmail.com>
2018-11-20 22:53:06 -0200

    Attack

---
+++ RogueRPG/Assets/Skills/Scripts/Attacks/Attack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Attack : ScriptableObject
+{
+    [Range(0f, 1f)]
+    protected float precision;
+    [Range(0f, 1f)]
+    protected float critic;
+    [Range(0.1f, 1f)]
+    protected float dmgIntensifier;
+    protected int range;
+
+    public abstract bool CanItReach(Character user, Tile target, Tile tile);
+}
