c033be7ba5bf807e242f39bd04f77cf53e61834b
Elton Mesquita <callmeelton@gmail.com>
2018-11-23 09:29:18 -0300

    Move as a Assist

---
+++ RogueRPG/Assets/Skills/Scripts/Move/Move.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : Assist
+{
+    public override void Act(Character user, Tile target, SkillAnimation animationPrefab)
+    {
+        target.SetCharacter(user);
+    }
+
+    public override bool IsTargetable(Character user, Tile tile) { return Mathf.Abs(user.GetPosition() - tile.GetRow()) <= range && user.IsPlayable() != tile.GetSide(); }
+    public override bool WillBeAffected(Character user, Tile target, Tile tile) { return target == tile; }
+}
