8adebcf8de6dfb7051efb577ba0994e2fa8511c8
DESKTOP-K49PGQT\MarcosMourão <marcosarruda.m@gmail.com>
2019-01-30 11:09:08 -0200

    Dungeon Btn

---
+++ RogueRPG/Assets/Map/DungeonBtn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DungeonBtn : MonoBehaviour
+{
+    Text dungeonName;
+    Text dungeonLevel;
+    Button button;
+    [SerializeField]
+    Dungeon dungeon;
+
+    private void Awake()
+    {
+        gameObject.SetActive(false);
+        Appear();
+    }
+
+    public void Appear()
+    {
+        gameObject.SetActive(true);
+
+        var texts = GetComponentsInChildren<Text>();
+        dungeonName = texts[0];
+        dungeonLevel = texts[1];
+        button = GetComponentInChildren<Button>();
+
+        dungeonName.text = dungeon.GetDungeonName();
+        dungeonLevel.text = "Level " + dungeon.GetLevel();
+    }
+
+    public void OnBtnClick()
+    {
+        GameManager.getInstance().GoToDungeon(dungeon);
+    }
+}
--- RogueRPG/Assets/StartUI.cs
+++ RogueRPG/Assets/StartUI.cs
@@ -4,14 +4,6 @@ using UnityEngine;
 
 public class StartUI : MonoBehaviour
 {
-
-    //public void StartMission(int index)
-    //{
-    //    GameManager gameManager = FindObjectOfType<GameManager>();
-    //    //gameManager.ChangeSelectedQuest(index);
-    //    //gameManager.LoadDungeonScene();
-    //}
-
     public void GoToDungeon(Dungeon dungeon)
     {
         GameManager.getInstance().GoToDungeon(dungeon);
