1dc16d322125aa1987452476106903bdddd6354c
DESKTOP-K49PGQT\MarcosMourão <marcosarruda.m@gmail.com>
2019-02-11 16:39:04 -0200

    Fog of War in the Map

--- RogueRPG/Assets/Map/DungeonBtn.cs
+++ RogueRPG/Assets/Map/DungeonBtn.cs
@@ -10,10 +10,17 @@ public class DungeonBtn : MonoBehaviour
     Text dungeonLevel;
     Button button;
     Dungeon dungeon;
+    ParticleSystem particleSystem;
 
     void Start()
     {
-        gameObject.SetActive(false);
+        particleSystem = GetComponentInChildren<ParticleSystem>();
+
+        var texts = GetComponentsInChildren<Text>();
+        dungeonName = texts[0];
+        dungeonLevel = texts[1];
+        button = GetComponentInChildren<Button>();
+
         Initialize(GameManager.getInstance().GetDungeon(index));
     }
 
@@ -26,21 +33,20 @@ public class DungeonBtn : MonoBehaviour
         {
             case Dungeon.State.Open:
             case Dungeon.State.Beaten:
+                particleSystem.gameObject.SetActive(false);
                 break;
             case Dungeon.State.JustDiscovered:
                 //TODO iniciar a animaçãozinha feliz de ter sido acabado de ser descoberta
+                particleSystem.Stop();
                 this.dungeon.SetState(Dungeon.State.Open);
                 break;
             default:
+                dungeonName.gameObject.SetActive(false);
+                button.gameObject.SetActive(false);
+                dungeonLevel.gameObject.SetActive(false);
+                particleSystem.Play();
                 return;
         }
-
-        gameObject.SetActive(true);
-
-        var texts = GetComponentsInChildren<Text>();
-        dungeonName = texts[0];
-        dungeonLevel = texts[1];
-        button = GetComponentInChildren<Button>();
 
         dungeonName.text = dungeon.GetDungeonName();
         dungeonLevel.text = "Level " + dungeon.GetLevel();
