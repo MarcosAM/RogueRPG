6fc223ca1ae8e29ec01c4f3abcf351eaac35acb8
DESKTOP-K49PGQT\MarcosMourão <marcosarruda.m@gmail.com>
2019-09-02 20:36:48 -0300

    End Scene

---
+++ RogueRPG/Assets/End/Credits.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    Animator animator;
+    CreditsListener listener;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Play(CreditsListener listener = null)
+    {
+        this.listener = listener;
+        if (animator)
+        {
+            animator.SetTrigger("play");
+        }
+        else
+        {
+            Stop();
+        }
+    }
+
+    void Stop()
+    {
+        if (this.listener != null)
+        {
+            listener.OnCreditsEnded();
+        }
+    }
+
+    public interface CreditsListener
+    {
+        void OnCreditsEnded();
+    }
+}
---
+++ RogueRPG/Assets/End/CreditsManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsManager : MonoBehaviour, Credits.CreditsListener
+{
+
+    Credits credits;
+
+    void Start()
+    {
+        credits = FindObjectOfType<Credits>();
+
+        if (credits)
+        {
+            credits.Play(this);
+        }
+        else
+        {
+            RestartGame();
+        }
+    }
+
+    void RestartGame()
+    {
+        GameManager gameManager = GameManager.getInstance();
+
+        if (gameManager)
+        {
+            gameManager.LoadScene(0);
+        }
+    }
+
+    public void OnCreditsEnded()
+    {
+        RestartGame();
+    }
+}
